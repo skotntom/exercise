a422de5b3a8d28c6b42656a1893a73e4d11c503f
skotntom <tomas.skotnica@tietoevry.com>
2023-05-09 12:42:46 +0200

    test string

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+string testString = "Check";
+using (HttpResponseMessage response = client.GetAsync("localhost:10000/foo").Result)
+{
+    response.EnsureSuccessStatusCode();
+    string responseBody = await response.Content.ReadAsStringAsync();
+    string sss = responseBody.ToString();
+
+    JToken token = JObject.Parse(sss);
+    int count = (int)token.SelectToken("count");
+    Assert(count, 1);
+
+    if (count > 0)
+    {
+        string bar = (string)token.SelectToken($"value[{0}].bar");
+        Assert(bar, testString);
+    }
+}
